7bb60dd12f5b4dbec69ed8e2f35506b20efbd8f7
Vitalii Boiarskyi2 <vitalii_boiarskyi2@epam.com>
2017-08-11 19:54:51 +0300

    Update sample bash script

--- integrate/update-sample.sh
+++ integrate/update-sample.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env bash
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 [source-bucket]"
+  echo ''
+  echo 'Options:'
+  echo '  source-bucket        S3 bucket name where data will be stored'
+  exit 1
+fi
+
+S3_BUCKET=$1
+
+aws cloudformation deploy --template-file 'sample.json' --stack-name "amazon-athena-as-code-sample" \
+  --parameter-overrides Bucket=${S3_BUCKET}
